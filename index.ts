42fa945ba4af0419b4942ee7fc613408f113991e
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-06-08 22:04:48 +0200

    add public folder

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
